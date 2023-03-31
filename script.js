0a7677bc282aa2bf6c71355b7dc4580428b1db06
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-31 15:18:54 -0700

    Created the styling for the grid in order to display them evenly

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     const container = document.querySelector("#container");
     let grid = [];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 16; i++) {
         grid[i] = [];
-        for (let j = 0; j < 4; j++) {
+        for (let j = 0; j < 16; j++) {
             grid[i][j] = document.createElement("div");
             grid[i][j].classList.add(`row-${i + 1}`);
             grid[i][j].classList.add(`col-${j + 1}`);
