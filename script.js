417b5598cc8f99733f6a00b506503fc7c3669556
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-31 16:32:43 -0700

    Attempted to change color of divs when mouse is over, however, unable to reference all divs. querySelectAll only works with staticly created elements, and getElementByClassName isn't seeming to work either

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ window.onload = () => {
         grid[i] = [];
         for (let j = 0; j < 16; j++) {
             grid[i][j] = document.createElement("div");
-            grid[i][j].classList.add(`row-${i + 1}`);
-            grid[i][j].classList.add(`col-${j + 1}`);
+            grid[i][j].classList.add("grid-item");
+            grid[i][j].classList.add(`r-${i + 1}c-${j + 1}`);
             container.appendChild(grid[i][j]);
         }
     }
+    const divs = document.getElementsByClassName("grid-item");
+
+    divs.addEventListener("mouseover", () => divs.style.backgroundColor = "white");
+    divs.addEventListener("mouseout", () => divs.style.backgroundColor = "gray");
 };
