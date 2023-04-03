5dad12367e0f6653f51555e38e0b35a95c57f3aa
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-04-02 21:59:32 -0700

    Instead of attempting to fix the grid's sizing staticly, I dynamicly changed the grid's size on JavaScript, to get all squares to evenly fit in the container. This works for any sized grid.

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ window.onload = () => {
     for (let i = 0; i < 16; i++) {
         grid[i] = [];
         for (let j = 0; j < 16; j++) {
+            const side = 500 / 16;
             grid[i][j] = document.createElement("div");
             grid[i][j].classList.add("grid-item");
             grid[i][j].classList.add(`r-${i + 1}c-${j + 1}`);
+            grid[i][j].style.width = `${side}px`;
+            grid[i][j].style.height = `${side}px`;
             container.appendChild(grid[i][j]);
         }
     }
     const divs = document.querySelectorAll(".grid-item");
 
-    divs.forEach(cell => cell.addEventListener("mouseover", () => cell.style.backgroundColor = "white"));
+    divs.forEach(cell => cell.addEventListener("mouseover", () => cell.style.backgroundColor = "black"));
 };
